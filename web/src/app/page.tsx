669ef7fa4ed441504ebf8e51aa9e1d4a98b3710e
NishiP-creator <allp7821146@gmail.com>
2026-09-06 04:00:47 -0700

    Add Next.js site with Home and Project pages.

---
+++ web/src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+   <div>
+    <h1>Nishi Parameshwara</h1>
+    <p>Senior Software Engineer</p>
+    <p>Avalara Inc.</p>
+    <a href="mailto:niship007@gmail.com">niship007@gmail.com</a>
+    <a href="https://github.com/niship007"> GitHub</a>
+    <a href="https://linkedin.com/in/niship007"> LinkedIn</a>
+    <a href="/project"> Project</a>
+  </div>
+  )
+}
---
+++ web/src/app/project/page.tsx
@@ -0,0 +1,10 @@
+export default function Project() {
+    return (
+     <div>
+      <h1>Portfolio and Blog</h1>
+      <p>Personal Website and Diary</p>
+      <p>I built this website to showcase my projects and share my thoughts and experiences.</p>
+      <a href="/">Home</a>
+    </div>
+    )
+  }
